a79a6373b84ec0cfb341c546e7ad0d70e4c14cfb
uaquax <uaquax@tutanota.com>
2024-04-07 22:16:44 +0800

    new and signin

--- src/arch_client.rs
+++ src/arch_client.rs
@@ -2,7 +2,8 @@ use std::pin::{ pin, Pin };
 use std::future::Future;
 
 use futures_util::future::{ select, Either };
-use grammers_client::{ Client, Update };
+use grammers_client::{ Client, Config, SignInError, Update };
+use grammers_session::Session;
 use tokio::task;
 
 pub type Res = Result<(), Box<dyn std::error::Error + Send + Sync>>;
@@ -22,6 +23,60 @@ pub struct ArchClient {
 }
 
 impl ArchClient {
+    pub async fn new(cfg: ArchConfig) -> Self {
+        log::info!("Connecting to Telegram...");
+
+        let client = Client::connect(Config {
+            session: Session::load_file_or_create(&cfg.session_file).unwrap(),
+            api_id: cfg.api_id,
+            api_hash: cfg.api_hash.clone(),
+            params: Default::default(),
+        }).await.unwrap();
+        log::info!("Connected!");
+
+        Self {
+            client: client,
+            sign_out: false,
+            config: cfg,
+        }
+    }
+
+    pub async fn sign_in(&mut self) {
+        let mut sign_out = false;
+        if !self.client.is_authorized().await.unwrap() {
+            log::info!("Signing in...");
+
+            let phone = (self.config.prompt)("Enter your phone number (international format): ");
+            let token = self.client.request_login_code(&phone).await.unwrap();
+            let code = (self.config.prompt)("Enter the code you received: ");
+
+            let signed_in = self.client.sign_in(&token, &code).await;
+
+            match signed_in {
+                Err(SignInError::PasswordRequired(password_token)) => {
+                    let hint = password_token.hint().unwrap_or("None");
+                    let prompt_message = format!("Enter the password (hint {}): ", &hint);
+                    let password = (self.config.prompt)(prompt_message.as_str());
+
+                    self.client.check_password(password_token, password.trim()).await.unwrap();
+                }
+                Ok(_) => (),
+                Err(e) => panic!("{}", e),
+            }
+            log::info!("Signed in!");
+
+            match self.client.session().save_to_file(&self.config.session_file) {
+                Ok(_) => {}
+                Err(e) => {
+                    log::error!("failed to save the session, will sign out when done: {}", e);
+                    sign_out = true;
+                }
+            }
+        }
+
+        self.sign_out = sign_out;
+    }
+
     pub async fn start(self) {
         let client_handle = self.client.clone();
         let network_handle = task::spawn(async move { self.client.run_until_disconnected().await });
